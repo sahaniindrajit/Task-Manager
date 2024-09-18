deefbd88bb0f68460f6719b1faf19c1f3dcf08ea
Indrajit sahani <indrajit38mig@gmail.com>
2024-09-18 19:43:23 +0530

    added signup

---
+++ app/api/user/signup/route.ts
@@ -0,0 +1,64 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { UserSchema } from '@/zod/userTaskType';
+import bycrpt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { cookies } from 'next/headers'
+
+const prisma = new PrismaClient().$extends(withAccelerate())
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json()
+        const userData = UserSchema.parse(body)
+
+        if (!userData) {
+            return NextResponse.json({
+                msg: "Invalid data type"
+            }, { status: 200 });
+        }
+
+        const user = await prisma.user.findFirst({
+            where: {
+                email: userData.email
+            }
+        })
+        if (user) {
+            return NextResponse.json({
+                msg: "User already exists/Try logging in"
+            }, { status: 200 });
+        }
+        const hash = await bycrpt.hash(userData.password, 10);
+
+        const createUser = await prisma.user.create({
+            data: {
+                email: userData.email,
+                password: hash
+            }
+        })
+
+        const payLoad = {
+            username: createUser.email,
+            userID: createUser.id
+
+        }
+        const secert = process.env.NEXT_PUBLIC_JWT_PASSWORD
+
+        console.log(secert)
+        const token = jwt.sign(payLoad, secert);
+
+        cookies().set({
+            name: 'access_token',
+            value: token,
+            httpOnly: true,
+            path: '/',
+        })
+        return NextResponse.json({
+            msg: "User created succesfully"
+        });
+    } catch (err) {
+        console.error('Error:', err);
+        return NextResponse.json({ error: 'Failed to process request' }, { status: 500 });
+    }
+}
