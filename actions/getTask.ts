32b14f2f918f95705dcd5064c0ef490a82d12ea5
Indrajit sahani <indrajit38mig@gmail.com>
2024-09-21 14:32:24 +0530

    ts error

--- actions/getTask.ts
+++ actions/getTask.ts
@@ -4,20 +4,15 @@ import gettoken from './verifyToken';
 import prisma from '@/db';
 
 export default async function getTask() {
-    try {
-        const userId = await gettoken();
 
-        if (!userId) {
-            return { error: 'Not authenticated.' };
-        }
+    const userId = await gettoken();
 
-        const tasks = await prisma.task.findMany({
-            where: { userId: userId },
-        });
 
-        return tasks;
-    } catch (err) {
-        console.error('Error fetching tasks:', err);
-        return { error: 'Failed to fetch tasks.' };
-    }
+
+    const tasks = await prisma.task.findMany({
+        where: { userId: userId },
+    });
+
+    return tasks;
+
 }
